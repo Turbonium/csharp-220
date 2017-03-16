1ab9465384bb5f16c9cde42c40ccbd8a86fbbf40
Mike Kett <mikett@microsoft.com>
2017-03-16 14:01:10 -0700

    Linq list manipulation

---
+++ Assignment2ListsandMoreLists/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment2ListsandMoreLists.Models;
+
+namespace Assignment2ListsandMoreLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<Models.User>();
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            //Display Initial set of users:
+            var initial = string.Join(Environment.NewLine, users.Select(x => string.Format($"Name: {x.Name}, Password: {x.Password}")));
+
+            Console.WriteLine("Initial set of users:");
+            Console.WriteLine(initial);
+            Console.WriteLine("-----------------------------------------");
+
+
+            var onlyhello = string.Join(Environment.NewLine, users.Where(x => x.Password.ToUpper().Contains("HELLO")).Select(x=>string.Format($"Name: {x.Name}, Password: {x.Password}")));
+
+            Console.WriteLine($"Users which have a password of Hello:");
+            Console.WriteLine(onlyhello);
+            Console.WriteLine("-----------------------------------------");
+
+            //Remove users which have a password that matches their username in lowercase
+
+            var removePassword = users.ToList();
+            removePassword.RemoveAll(x => x.Name.ToLower().Contains(x.Password.ToLower()));
+            users = removePassword;
+            var removedMatchingPassword = string.Join(Environment.NewLine, users.Select(x => string.Format($"Name: {x.Name}, Password: {x.Password}")));
+
+            Console.WriteLine("Removed users where username equaled lowercase password:");
+            Console.WriteLine(removedMatchingPassword);
+            Console.WriteLine("-----------------------------------------");
+
+            //Remove first user which has a password of hello
+
+            var removeFirstHello = users.ToList();
+            removeFirstHello.Remove(removeFirstHello.First(x=>x.Password.ToUpper().Contains("HELLO")));
+            users = removeFirstHello;
+            var removedFirstHello = string.Join(Environment.NewLine, users.Select(x => string.Format($"Name: {x.Name}, Password: {x.Password}")));
+
+            //Of the remaining users, remove the first user which has a password of hello
+            Console.WriteLine("Of the Remaining users, Removed first user with a password of hello:");
+            Console.WriteLine(removedFirstHello);
+            Console.WriteLine("-----------------------------------------");
+
+        }
+    }
+}
