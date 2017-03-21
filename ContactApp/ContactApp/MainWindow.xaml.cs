1e76831b0e68dbd7181b1486dc20dc9835d500d0
DESKTOP-E55BUP2\Turbo <mgkett@gmail.com>
2017-03-20 20:53:40 -0700

    Added File delete and context menu delete

--- ContactApp/ContactApp/MainWindow.xaml.cs
+++ ContactApp/ContactApp/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ContactApp.Models;
+using System.ComponentModel;
 
 namespace ContactApp
 {
@@ -21,6 +22,10 @@ namespace ContactApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+        private ContactModel selectedContact;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -45,6 +50,88 @@ namespace ContactApp
             uxContactList.ItemsSource = contacts
                 .Select(t => ContactModel.ToModel(t))
                 .ToList();
+        }
+
+        private void uxFileChange_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void uxFileDelete_Click(object sender, RoutedEventArgs e)
+        {
+            App.ContactRepository.Remove(selectedContact.Id);
+            selectedContact = null;
+            LoadContacts();
+        }
+
+        private void ColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxContactList.ItemsSource);
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                view.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            view.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+        public class SortAdorner : Adorner
+        {
+            private static Geometry ascGeometry =
+                    Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z");
+
+            private static Geometry descGeometry =
+                    Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
+
+            public ListSortDirection Direction { get; private set; }
+
+            public SortAdorner(UIElement element, ListSortDirection dir)
+                    : base(element)
+            {
+                this.Direction = dir;
+            }
+
+            protected override void OnRender(DrawingContext drawingContext)
+            {
+                base.OnRender(drawingContext);
+
+                if (AdornedElement.RenderSize.Width < 20)
+                    return;
+
+                TranslateTransform transform = new TranslateTransform
+                        (
+                                AdornedElement.RenderSize.Width - 15,
+                                (AdornedElement.RenderSize.Height - 5) / 2
+                        );
+                drawingContext.PushTransform(transform);
+
+                Geometry geometry = ascGeometry;
+                if (this.Direction == ListSortDirection.Descending)
+                    geometry = descGeometry;
+                drawingContext.DrawGeometry(Brushes.Black, null, geometry);
+
+                drawingContext.Pop();
+            }
+        }
+
+        private void uxContactList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedContact = (ContactModel) uxContactList.SelectedValue;
+        }
+
+        private void uxFileDelete_Loaded(object sender, RoutedEventArgs e)
+        {
+            uxFileDelete.IsEnabled = (selectedContact != null);
+            uxContextFileDelete.IsEnabled = (selectedContact != null);
         }
     }
 }
